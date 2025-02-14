853ce0fce3a58e3040072c9f95a8bef8b9a9e1ef
Alexey Bukhtin <bukhtin@gmail.com>
2025-02-14 23:43:01 +0100

    Add a player demo for SystemSlider

--- Demo/Sources/Demo/SystemDemo.swift
+++ Demo/Sources/Demo/SystemDemo.swift
@@ -9,6 +9,10 @@ import CompactSlider
 struct CompactSliderSystemDemo: View {
     @State private var progress: Double = 0.1
     
+    @State private var playerTime: TimeInterval = 10
+    @State private var isPlaying = false
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack(spacing: 20) {
             Text("\(progress, format: .percent.precision(.fractionLength(1)))")
@@ -25,11 +29,7 @@ struct CompactSliderSystemDemo: View {
             SystemSlider(value: $progress, step: 0.1)
             
             Text("\"System\" expandable slider")
-            
-            SystemSlider(value: $progress)
-                .systemSliderStyle(handleStyle: .hidden())
-                .compactSliderOptionsByAdding(.expandOnFocus(minScale: 0.4))
-                .compactSliderAnimation(.bouncy, when: .dragging, .hovering)
+            playerView()
             
             Text("\"System\" scrollable slider")
             
@@ -51,6 +51,52 @@ struct CompactSliderSystemDemo: View {
         .padding()
         .compactSliderOptionsByAdding(.scrollWheel, .tapToSlide)
         .compactSliderAnimation(.bouncy(duration: 0.2, extraBounce: 0.05), when: .tapping)
+    }
+    
+    private func playerView() -> some View {
+        GroupBox {
+            SystemSlider(value: $playerTime, in: 0...60)
+                .systemSliderStyle(handleStyle: .hidden())
+                .compactSliderOptionsByAdding(.expandOnFocus(minScale: 0.5))
+                .compactSliderAnimation(.bouncy, when: .dragging, .hovering)
+            
+            if #available(macOS 13.0, iOS 16.0, *) {
+                HStack(alignment: .top) {
+                    Text("\(Duration(secondsComponent: Int64(playerTime), attosecondsComponent: 0), format: .time(pattern: .minuteSecond(padMinuteToLength: 2)))")
+                        .contentTransition(.numericText())
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                    
+                    Button(action: { isPlaying.toggle() }) {
+                        Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+                    .font(.title)
+                    .padding(4)
+                    
+                    Spacer()
+                    Text("\(Duration(secondsComponent: Int64(60 - playerTime), attosecondsComponent: 0), format: .time(pattern: .minuteSecond(padMinuteToLength: 2)))")
+                        .contentTransition(.numericText(countsDown: true))
+                        .foregroundStyle(.secondary)
+                }
+                .font(.footnote)
+                .monospacedDigit()
+                .padding(.horizontal, 6)
+            }
+        }
+        .onReceive(timer) { _ in
+            guard isPlaying else { return }
+            
+            withAnimation {
+                if playerTime >= 59 {
+                    isPlaying = false
+                    playerTime = 60
+                    return
+                }
+                
+                playerTime += 1
+            }
+        }
     }
 }
 
